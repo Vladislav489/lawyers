386be28f52f910af957c694c20c6e7e4d016ed0d
Максим <anatolev.web@gmail.com>
2023-11-28 21:03:43 +1000

    create logic-models

---
+++ app/Models/CoreEngine/LogicModels/User/UserLogic.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models\CoreEngine\LogicModels\User;
+
+use App\Models\CoreEngine\Core\CoreEngine;
+use App\Models\CoreEngine\Model\InformationCategoryName;
+
+class UserLogic extends CoreEngine
+{
+    private $helpEngine;
+
+    public function __construct($params = [], $select = ['*'], $callback = null)
+    {
+        $this->helpEngine['InformationCategoryName'] = new InformationCategoryName();
+        $this->query = $this->engine->newQuery();
+        $this->getFilter();
+        $this->compileGroupParams();
+
+        parent::__construct($params, $select);
+    }
+
+    protected function compileGroupParams(): array
+    {
+        $this->group_params = [
+            'select' => [],
+            'by' => [],
+            'relatedModel' => []
+        ];
+
+        return $this->group_params;
+    }
+
+    protected function defaultSelect(): array
+    {
+        $tab = $this->engine->tableName();
+        $this->default = [];
+
+        return $this->default;
+    }
+
+    protected function getFilter(): array
+    {
+        $tab = $this->engine->getTable();
+        $this->filter = [];
+        $this->filter = array_merge($this->filter, parent::getFilter());
+
+        return $this->filter;
+    }
+}
--- database/migrations/employee/2023_11_21_000009_create_user_employee_comment_table.php
+++ database/migrations/employee/2023_11_21_000009_create_user_employee_comment_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->nullable();
-            $table->text('comment_text')->comment('текст комментария');
+            $table->text('text')->comment('текст комментария');
             $table->boolean('is_deleted')->default(false);
             $table->boolean('is_archive')->default(false)->comment('в архиве');
             $table->bigInteger('user_id')->unsigned()->index()->comment('ID пользователя');
