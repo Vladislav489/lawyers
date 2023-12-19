610df5697e6f0dd152e4421e535b10dcb1cd4b5e
alexbrik8888 <itcyberdynesystems@gmail.com>
2023-12-19 15:47:39 +0300

    fix page

--- resources/views/lawyers/client/vacancy-list.blade.php
+++ resources/views/lawyers/client/vacancy-list.blade.php
@@ -12,21 +12,16 @@
                         'params_component' => [
                             'autostart' => 'true',
                             'name' => 'vacancy_list',
-                            'url' => route__("actionGetListSite_backcontroller"),
+                            'url' => route__("actionGetVacancyList_clientmainstaycontroller"),
 
-                            'template' => '<div v-for="item in data" class="card mt-3 border-primary">
-                                <div class="card-body" v-bind:data-id="@{{ item.id }}">
-                                    <h5 class="card-title">Description: @{{ item.description }}</h5>
-                                    <p class="card-text">Payment: @{{ item.payment }} &#8381;</p>
-                                    <a href="'.route__("actionEditVacancy_clientcontroller").'"?id=@{{ item.id }}" class="btn btn-secondary">Редактировать</a>
-                                    <button
-                                        type="button"
-                                        class="btn btn-danger"
-                                        style="pointer-events: all;"
-                                        data-text="Удаляю"
-                                    >Удалить</button>
+                            'template' => "<div v-for='item in data' class='card mt-3 border-primary'>
+                                <div class='card-body' v-bind:id='item.id'>
+                                    <h5 class='card-title'>Description: @{{ item.description }}</h5>
+                                    <p class='card-text'>Payment: @{{item.payment}} &#8381;</p>
                                 </div>
-                            </div>',
+                            </div>",
+
+                            'autostart' => 'true',
                             'pagination' => [
                                 'page' => 1,
                                 'pageSize' => 14,
--- resources/views/lawyers/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="container flex-between">
-    <p class="footer__text">© {{date("Y")}} SignatorInvestors.com</p>
-        <ul class="footer-item flex">
-            <li class="footer-list"><a href="/about" class="footer-list__link">About us</a></li>
-            <li class="footer-list"><a href="/police" class="footer-list__link">Privacy policy</a></li>
-        </ul>
-</div>
