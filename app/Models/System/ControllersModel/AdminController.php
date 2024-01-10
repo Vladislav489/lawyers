c9e7471c51160f7f0795023a7815ee195fd9b2d9
alexbrik8888 <108339952+alexbrik8888@users.noreply.github.com>
2024-01-10 17:39:35 +0300

    Update AdminController.php

--- app/Models/System/ControllersModel/AdminController.php
+++ app/Models/System/ControllersModel/AdminController.php
@@ -6,9 +6,10 @@ use Illuminate\Support\Facades\Auth;
 
 class AdminController extends CentralController {
     public function callAction($method, $parameters){
-       // if (!Auth::check()) {
+        if (!Auth::check()) {
+            dd("11");
            //     return redirect(route__("actionIndex_logincontroller"));
-       // }
+        }
         $routs =  (new  Routs([],['id','name_title','url','open','site_id','physically','active']))->getRoute(
             request()->getPathInfo(),
             \Illuminate\Support\Facades\Route::current()->uri());
