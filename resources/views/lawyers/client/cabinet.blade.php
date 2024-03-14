9164bb5a2fc48834be832b5b370f4a1918a10e3b
martyn12 <87023187+martyn12@users.noreply.github.com>
2024-03-14 12:47:02 +0400

    views for client cabinet and payments

--- resources/views/lawyers/client/cabinet.blade.php
+++ resources/views/lawyers/client/cabinet.blade.php
@@ -2,11 +2,217 @@
 @section('title', 'Кабинет клиента')
 
 @section('content')
-    <section class="mt-5">
+    <section class="gradient-bg u-container lawyer-section">
         <div class="container">
-            <div class="ro">
-                <div class="col">
-                    <h1 class="fs-3">Кабинет клиента</h1>
+            <div class="lawyer-container">
+                <div class="left">
+                    <div class="lawyer-block">
+                        <div class="lawyer-top">
+                            <div class="lawyer-img">
+                                <img src="/lawyers/images/main/lawyer-img.png" alt="lawyer-img">
+                            </div>
+
+                            <div class="lawyer-info">
+                                <h2 class="lawyer-name">Соколовский Владимир Александрович</h2>
+
+                                <span class="lawyer-check">Проверенный юрист
+                                    <img src="/lawyers/images/icons/check-icon-white.svg" alt="check-icon" class="icon">
+                                </span>
+                            </div>
+                        </div>
+
+                        <div class="lawyer-bottom">
+                            <div class="lawyer_rate-block">
+                                <div class="specialist-rate">
+                                    <div class="stars">
+                                        <img src="/lawyers/images/icons/star-icon-full.svg" alt="star-icon">
+                                        <img src="/lawyers/images/icons/star-icon-full.svg" alt="star-icon">
+                                        <img src="/lawyers/images/icons/star-icon-full.svg" alt="star-icon">
+                                        <img src="/lawyers/images/icons/star-icon-full.svg" alt="star-icon">
+                                        <img src="/lawyers/images/icons/star-icon-empty.svg" alt="star-icon">
+                                    </div>
+
+                                    <span>32 ответа</span>
+                                </div>
+
+                                <div class="specialist-perm">
+                                    <p>Право рейтинг:</p>
+                                    <span>4.0</span>
+                                    <img src="/lawyers/images/icons/info-icon-blue.svg" alt="info-icon" class="icon">
+                                </div>
+                            </div>
+
+                            <div class="lawyer-info">
+                                <div class="lawyer-info_row">
+                                    <img src="/lawyers/images/icons/loc-icon-blue.svg" alt="loc-icon" class="icon">
+
+                                    <span>Москва и МО</span>
+                                </div>
+                                <div class="lawyer-info_row">
+                                    <img src="/lawyers/images/icons/bah-icon-blue.svg" alt="bah-icon" class="icon">
+
+                                    <span>15 лет практики</span>
+                                </div>
+                                <div class="lawyer-info_row">
+                                    <img src="/lawyers/images/icons/phone-icon-blue.svg" alt="phone-icon" class="icon">
+
+                                    <a href="tel:+7 (999) 999 99 99" class="link">+7 (999) 999 99 99</a>
+                                </div>
+                                <div class="lawyer-info_row">
+                                    <img src="/lawyers/images/icons/planet-icon-blue.svg" alt="planet-icon" class="icon">
+
+                                    <a href="#" class="link">www.site.ru</a>
+                                </div>
+                                <div class="lawyer-info_row">
+                                    <img src="/lawyers/images/icons/message-icon-blue.svg" alt="message-icon" class="icon">
+
+                                    <span>Консультации онлайн:</span>
+                                    <span class="bold">Да</span>
+                                </div>
+                                <div class="lawyer-info_row">
+                                    <img src="/lawyers/images/icons/clock-icon-blue.svg" alt="clock-icon" class="icon">
+
+                                    <span>Время работы:</span>
+                                    <time class="bold">Пн-Сб 9:00 - 18:00</time>
+                                </div>
+                            </div>
+
+                            <button type="button" class="main-btn main-btn_white">
+                                <span class="first">Редактировать</span>
+                                <span class="second">Редактировать</span>
+                            </button>
+
+                            <div class="balance-block">
+                                <div class="balance-block_left">
+                                    <p class="balance-text">Ваш баланс</p>
+                                    <p class="balance_balance">
+                                        <span>&#8381</span>
+                                        0 руб
+                                    </p>
+                                </div>
+
+                                <a href="#" class="main-btn">
+                                    <span class="first">
+                                        Пополнить
+                                        <img src="/lawyers/images/icons/arrow-icon-white.svg" alt="arrow-icon" class="icon">
+                                    </span>
+                                    <span class="second">
+                                        Пополнить
+                                        <img src="/lawyers/images/icons/arrow-icon-white.svg" alt="arrow-icon" class="icon">
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="right">
+                    <ul class="round-top_nav">
+                        <li class="active"><button type="button">Мои заказы</button></li>
+                        <li><button type="button">Услуги</button></li>
+                        <li><button type="button">Отзывы</button></li>
+                        <li><button type="button">Ответы юриста</button></li>
+                    </ul>
+
+                    <div class="my-orders lawyer-wrapper">
+                        <h2 class="lawyer-wrapper_title">Мои заказы <span>1/2</span></h2>
+
+                        <ul class="my-orders_ul">
+                            <li>
+                                <div class="my-orders_info">
+                                    <p class="my-orders_text">
+                                        Оглавление вопроса, который задал клиент... Оглавление вопроса, который задал
+                                        клиент... Оглавление вопроса, который задал клиент... Оглавление вопроса,
+                                        который задал клиент...
+                                    </p>
+                                    <p class="my-orders_price">
+                                        Бесплатный
+                                        <span>0 ответов</span>
+                                    </p>
+                                </div>
+
+                                <p class="my-orders_stage moderation-status">
+                                    модерация
+                                </p>
+                            </li>
+                            <li>
+                                <div class="my-orders_info">
+                                    <p class="my-orders_text">
+                                        Оглавление вопроса, который задал клиент... Оглавление вопроса, который задал
+                                        клиент... Оглавление вопроса, который задал клиент... Оглавление вопроса,
+                                        который задал клиент...
+                                    </p>
+                                    <p class="my-orders_price">
+                                        Бесплатный
+                                        <span>0 ответов</span>
+                                    </p>
+                                </div>
+
+                                <p class="my-orders_stage closed-status">
+                                    закрыт
+                                </p>
+                            </li>
+                        </ul>
+
+                        <button class="more-services">Еще 2 услуги</button>
+                    </div>
+
+                    <div class="lawsuit lawyer-wrapper">
+                        <h2 class="lawyer-wrapper_title lawyer-wrapper_title-left">Коллективные иски </h2>
+
+                        <ul class="my-orders_ul">
+                            <li>
+                                <div class="my-orders_info">
+                                    <p class="my-orders_text">
+                                        Иск собственников многоквартирного дома в признании права общей долевой
+                                        собственности на подвальные помещения здания
+                                    </p>
+
+                                    <ul class="my-orders_sub-ul">
+                                        <li>5 заявителей (Ожидают подтверждение 4)</li>
+                                        <li>5 сообщений</li>
+                                        <li>3 предложения от юристов</li>
+                                    </ul>
+                                </div>
+
+                                <a href="#" type="button" class="main-btn main-btn_white">
+                                    <span class="first">Открыть</span>
+                                    <span class="second">Открыть</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <div class="my-orders_info">
+                                    <p class="my-orders_text">
+                                        Иск собственников многоквартирного дома в признании права общей долевой
+                                        собственности на подвальные помещения здания
+                                    </p>
+
+                                    <ul class="my-orders_sub-ul">
+                                        <li>5 заявителей (Ожидают подтверждение 4)</li>
+                                        <li>5 сообщений</li>
+                                        <li>3 предложения от юристов</li>
+                                    </ul>
+                                </div>
+
+                                <a href="#" type="button" class="main-btn main-btn_white">
+                                    <span class="first">Открыть</span>
+                                    <span class="second">Открыть</span>
+                                </a>
+                            </li>
+                        </ul>
+
+                        <button class="more-services">Еще 2 услуги</button>
+                    </div>
+
+                    <div class="subscribe-block lawyer-wrapper">
+                        <h2 class="lawyer-wrapper_title lawyer-wrapper_title-left">Подписка</h2>
+
+                        <div class="subscribe">
+                            <p class="subscribe_text">SOS Поддержка</p>
+                            <p class="subscribe-status">Активна</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ resources/views/lawyers/client/payment.blade.php
@@ -0,0 +1,157 @@
+@extends('lawyers.layouts.main')
+@section('title', 'Пополнить баланс')
+
+@section('content')
+<section class="gradient-bg u-container payment-section">
+    <div class="container">
+        <ul class="breadcrumbs mobile-hidden">
+            <li class="cool-underline"><a href="#">Юрист</a></li>
+            <li class="cool-underline"><a href="#">Город</a></li>
+        </ul>
+
+        <div class="payment">
+            <div class="payment-block">
+                <h2 class="payment_header">
+                    Финансы Клиента
+                </h2>
+
+                <div class="payment-container finance-block">
+                    <div class="finance-line">
+                        <p>Доступно</p>
+                        <p>0 рублей</p>
+                    </div>
+                    <div class="finance-line">
+                        <p>Блокировано</p>
+                        <p>0 рублей</p>
+                    </div>
+                    <span class="line"></span>
+                    <div class="finance-line">
+                        <p>Ваш баланс</p>
+                        <p>0 рублей</p>
+                    </div>
+                    <div class="finance-line text-gray">
+                        <p>Ожидается</p>
+                        <p>0 рублей</p>
+                    </div>
+                    <div class="finance-line">
+                        <p>Сумма к оплате</p>
+                        <p>0 рублей</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="payment-block">
+                <h2 class="payment_header">
+                    Пополнение счета
+                </h2>
+
+                <form action="#" class="payment-container">
+                    <label>
+                        <span class="label_text">Сумма пополнения</span>
+                        <input type="text" placeholder="Сумма" name="execution-sum">
+                        <span class="label_subtext">рублей</span>
+                    </label>
+
+                    <div class="payment-type select">
+                        <label class="select-btn">
+                            <span class="label_text">Способ оплаты</span>
+                            <input type="text" placeholder="Выберите способ оплаты" readonly>
+                        </label>
+
+                        <ul class="select-window">
+                            <li>Банковской картой</li>
+                            <li>Наличными</li>
+                            <li>QIWI кошелек</li>
+                        </ul>
+
+                        <img src="/lawyers/images/icons/arrow-icon-gray.svg" alt="arrow-icon" class="icon sub-icon">
+
+                        <p class="code">
+                            <a href="#">Ввести код</a>
+                            <span>(если есть)</span>
+                        </p>
+                    </div>
+
+                    <p>
+                        <span class="label_text">Сумма к оплате</span>
+                        <span>0 рублей</span>
+                    </p>
+
+                    <p class="text_lil">
+                        <span class="label_text">Включая сбор за финансовую транзакцию</span>
+                        <span>0 рублей</span>
+                    </p>
+
+                    <button type="submit" class="main-btn">
+                        <span class="first">Продолжить <img src="/lawyers/images/icons/arrow-icon-white.svg" alt="arrow-icon"></span>
+                        <span class="second">Продолжить <img src="/lawyers/images/icons/arrow-icon-white.svg" alt="arrow-icon"></span>
+                    </button>
+                </form>
+            </div>
+
+            <div class="payment-block transaction-block">
+                <h2 class="payment_header">
+                    История транзакций
+                </h2>
+
+                <table class="transaction-history" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <th>ID</th>
+                        <th>Дата начала</th>
+                        <th>Дата завершения</th>
+                        <th>Сумма</th>
+                        <th>Статус</th>
+                        <th>Тип Транзакции</th>
+                        <th>Комментарий</th>
+                    </tr>
+                    <tr>
+                        <td>6784563</td>
+                        <td>12:27 03.11.2018</td>
+                        <td>13:44 03.11.2018</td>
+                        <td class="transaction-sum red">- 800 руб.</td>
+                        <td>Завершена</td>
+                        <td>Расходы</td>
+                        <td>Пополнение счета</td>
+                    </tr>
+                    <tr>
+                        <td>6784563</td>
+                        <td>12:27 03.11.2018</td>
+                        <td>13:44 03.11.2018</td>
+                        <td class="transaction-sum green">+ 800 руб.</td>
+                        <td>Завершена</td>
+                        <td>Расходы</td>
+                        <td>Оплата платного вопроса</td>
+                    </tr>
+                    <tr>
+                        <td>6784563</td>
+                        <td>12:27 03.11.2018</td>
+                        <td>13:44 03.11.2018</td>
+                        <td class="transaction-sum green">+ 800 руб.</td>
+                        <td>Завершена</td>
+                        <td>Расходы</td>
+                        <td>Оплата платного вопроса</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+        <div class="lawyer-block">
+            <div class="lawyer-avatar">
+                <img src="/lawyers/images/main/lawyer-img.png" alt="lawyer-avatar" class="lawyer-avatar">
+
+                <h3 class="lawyer-name">Соколовский Владимир Александрович</h3>
+            </div>
+
+            <div class="balance-block">
+                <div class="balance-block_left">
+                    <p class="balance-text">Ваш баланс</p>
+                    <p class="balance_balance">
+                        <span>&#8381</span>
+                        0 руб
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
